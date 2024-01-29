d78a7afff0bec80a06f7ef746d77b141ff3c1ba0
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-29 15:52:46 -0800

    Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,44 +1,58 @@
 #!/usr/bin/python3
+"""simple flask app
+"""
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
+    """root route
+    """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    """hbnb
+    """
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    return 'C {}'.format(text.replace('_', ' '))
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """c what
+    """
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
-    return 'Python {}'.format(text.replace('_', ' '))
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """python is cool
+    """
+    return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return '{} is a number'.format(n)
+@app.route("/number/<int:n>", strict_slashes=False)
+def intnumber(n):
+    """accept integer
+    """
+    return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('6-number_template.html', number=n)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def int_template(n):
+    """only display when n is integer
+    """
+    return render_template('5-number.html', number=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', number=n, odd_even='odd' if n % 2 != 0 else 'even')
-
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_or_even(n):
+    """only display when n is integer
+    """
+    return render_template('6-number_odd_or_even.html', number=n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
