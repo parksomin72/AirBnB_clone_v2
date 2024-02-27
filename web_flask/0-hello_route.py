3114b26d6a63c5b70c0521dd51cb7057985177dc
parksomin72 <fatimazahraealawi@gmil.com>
2024-02-28 00:53:10 +0100

    Set up development with Python

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,11 +8,29 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return 'Hello HBNB!'
+        """Displays 'Hello HBNB!'"""
+            return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+        if __name__ == "__main__":
+                app.run(host='0.0.0.0', port=5000)#!/usr/bin/python3
+                """
+                Starts a Flask web application.
+                """
+
+                from flask import Flask
+
+                app = Flask(__name__)
+
+
+                @app.route('/airbnb-onepage/', strict_slashes=False)
+                def hello_hbnb():
+                        """Displays 'Hello HBNB!'"""
+                            return 'Hello HBNB!'
+
+
+                        if __name__ == "__main__":
+                                app.run(host='0.0.0.0', port=5000)
+
