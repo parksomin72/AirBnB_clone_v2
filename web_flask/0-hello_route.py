8e99d55330ef8f49be9bc8831bf65c481e9c27ec
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-28 15:19:45 +0100

    .

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Define the route for the root URL '/'
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
-        """
-            Returns a message when accessing the route /airbnb-onepage/
-                """
-                    return "Hello HBNB!"
+    """
+    Returns a message when accessing the route /airbnb-onepage/
+    """
+    return "Hello HBNB!"
 
-                if __name__ == "__main__":
-                        app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    #Start the Flask development server
+    #Listen on all available network interfaces(0.0.0.0) and port 5000
+    app.run(host='0.0.0.0', port=5000)
