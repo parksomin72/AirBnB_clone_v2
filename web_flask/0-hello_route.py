941b6af9c5f8fe66f50b8f08dc37f9b59a920030
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-27 16:02:35 -0800

    new commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,27 +10,9 @@ app = Flask(__name__)
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
-        """Displays 'Hello HBNB!'"""
-            return 'Hello HBNB!'
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
 
 
-        if __name__ == "__main__":
-                app.run(host='0.0.0.0', port=5000)#!/usr/bin/python3
-                """
-                Starts a Flask web application.
-                """
-
-                from flask import Flask
-
-                app = Flask(__name__)
-
-
-                @app.route('/airbnb-onepage/', strict_slashes=False)
-                def hello_hbnb():
-                        """Displays 'Hello HBNB!'"""
-                            return 'Hello HBNB!'
-
-
-                        if __name__ == "__main__":
-                                app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
